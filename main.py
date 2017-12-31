f1ee1aa1809d2e6cb4d2f4564392fc4ac1229e94
Yu <coderyy@outlook.com>
2018-01-01 04:39:10 +0800

    build directory

---
+++ main.py
@@ -0,0 +1,49 @@
+import sys
+sys.path.append("..")
+import yue
+from tool.config import Config
+
+
+
+
+if __name__ == '__main__':
+
+    print '='*80
+    print '   Yue: Library for Music Recommendation.   '
+    print '='*80
+    print 'CF-based Recommenders:'
+
+
+    print 'Content-based Recommenders:'
+
+
+
+    print 'Hybrid Recommenders:'
+
+
+    print 'Baselines:'
+    print 'b1 MostPop   b2. Rand'
+    print '='*80
+    algor = -1
+    conf = -1
+    order = raw_input('please enter the num of the algorithm to run it:')
+    import time
+    s = time.time()
+
+
+    if order == '1':
+        conf = Config('../config/UserKNN.conf')
+
+    elif order == 'b1':
+        conf = Config('../config/MostPop.conf')
+
+    elif order == 'b2':
+        conf = Config('../config/rand.conf')
+
+    else:
+        print 'Error num!'
+        exit(-1)
+    musicSys = yue.Yue(conf)
+    musicSys.execute()
+    e = time.time()
+    print "Run time: %f s" % (e - s)
---
+++ yue.py
@@ -0,0 +1,7 @@
+
+class Yue(object):
+    def __init__(self,conf):
+        pass
+
+    def execute(self):
+        pass
