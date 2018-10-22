548e13465bd150ecc11b2edef40359e6efd8ebdf
Junwei Zhang <474650206@qq.com>
2018-10-22 21:27:47 +0800

    Update IterativeRecommender.py
    python 3.5

--- base/IterativeRecommender.py
+++ base/IterativeRecommender.py
@@ -28,14 +28,14 @@ class IterativeRecommender(Recommender):
 
     def printAlgorConfig(self):
         super(IterativeRecommender, self).printAlgorConfig()
-        print 'Reduced Dimension:',self.k
-        print 'Maximum Iteration:',self.maxIter
-        print 'Regularization parameter: regU %.3f, regI %.3f, regB %.3f' %(self.regU,self.regI,self.regB)
-        print '='*80
+        print ('Reduced Dimension:',self.k)
+        print ('Maximum Iteration:',self.maxIter)
+        print ('Regularization parameter: regU %.3f, regI %.3f, regB %.3f' %(self.regU,self.regI,self.regB))
+        print ('='*80)
 
     def initModel(self):
-        self.P = np.random.rand(self.data.getSize('user'), self.k)/10 # latent user matrix
-        self.Q = np.random.rand(self.data.getSize(self.recType), self.k)/10  # latent item matrix
+        self.P = np.random.rand(self.data.getSize('user'), self.k).astype(np.float32)/10 # latent user matrix
+        self.Q = np.random.rand(self.data.getSize(self.recType), self.k).astype(np.float32)/10 # latent item matrix
         self.loss, self.lastLoss = 0, 0
 
     def saveModel(self):
@@ -62,10 +62,10 @@ class IterativeRecommender(Recommender):
     def isConverged(self,iter):
         from math import isnan
         if isnan(self.loss):
-            print 'Loss = NaN or Infinity: current settings does not fit the recommender! Change the settings and try again!'
+            print ('Loss = NaN or Infinity: current settings does not fit the recommender! Change the settings and try again!')
             exit(-1)
         deltaLoss = (self.lastLoss-self.loss)
-        print '%s %s iteration %d: loss = %.4f, delta_loss = %.5f learning_Rate = %.5f' %(self.algorName,self.foldInfo,iter,self.loss,deltaLoss,self.lRate)
+        print ('%s %s iteration %d: loss = %.4f, delta_loss = %.5f learning_Rate = %.5f' %(self.algorName,self.foldInfo,iter,self.loss,deltaLoss,self.lRate))
         #check if converged
         cond = abs(deltaLoss) < 1e-3
         converged = cond
@@ -82,7 +82,7 @@ class IterativeRecommender(Recommender):
         N = max(top)
 
         if N > 100 or N < 0:
-            print 'N can not be larger than 100! It has been reassigned with 10'
+            print ('N can not be larger than 100! It has been reassigned with 10')
             N = 10
 
         res.append('userId: recommendations in (itemId, ranking score) pairs, * means the item matches.\n')
@@ -126,7 +126,7 @@ class IterativeRecommender(Recommender):
                 if recommendations[r] < itemSet[item]:
                     while True:
 
-                        mid = (l + r) / 2
+                        mid = (l + r) // 2
                         if recommendations[mid] >= itemSet[item]:
                             l = mid + 1
                         elif recommendations[mid] < itemSet[item]:
@@ -145,10 +145,10 @@ class IterativeRecommender(Recommender):
             recList[user] = resNames
 
             if i % 100 == 0:
-                print self.algorName, self.foldInfo, 'progress:' + str(i) + '/' + str(userCount)
+                print (self.algorName, self.foldInfo, 'progress:' + str(i) + '/' + str(userCount))
             for item in recList[user]:
                 line += item
-                if self.data.testSet[user].has_key(item):
+                if item in self.data.testSet[user]:
                     line += '*'
 
             line += '\n'
@@ -162,7 +162,7 @@ class IterativeRecommender(Recommender):
                 fileName = self.config['recommender'] + '@' + currentTime + '-top-' + self.ranking['-topN']\
                            + 'items' + self.foldInfo + '.txt'
             FileIO.writeFile(outDir, fileName, res)
-            print 'The result has been output to ', abspath(outDir), '.'
+            print ('The result has been output to ', abspath(outDir), '.')
         # output evaluation result
         outDir = self.output['-dir']
         fileName = self.config['recommender'] + '@' + currentTime + '-measure' + self.foldInfo + '.txt'
@@ -170,4 +170,68 @@ class IterativeRecommender(Recommender):
         self.measure = Measure.rankingMeasure(self.data.testSet, recList,top,self.data.getSize(self.recType))
 
         FileIO.writeFile(outDir, fileName, self.measure)
-        print 'The result of %s %s:\n%s' % (self.algorName, self.foldInfo, ''.join(self.measure))
+        print ('The result of %s %s:\n%s' % (self.algorName, self.foldInfo, ''.join(self.measure)))
+
+    def ranking_performance(self):
+        N = 10
+        itemcount = 0
+        recList = {}
+        testSample = {}
+        for user in self.data.testSet:
+            itemcount+=len(self.data.testSet[user])
+            if len(testSample) == 300:
+                break
+            testSample[user] = self.data.testSet[user]
+
+        for user in testSample:
+            itemSet = {}
+            predictedItems = self.predict(user)
+            for id, rating in enumerate(predictedItems):
+                itemSet[self.data.id2name['track'][id]] = rating
+
+            ratedList = self.data.testSet[user].keys()
+            ratingList = self.data.testSet[user].values()
+            for item in ratedList:
+                del itemSet[item]
+
+            Nrecommendations = []
+            for item in itemSet:
+                if len(Nrecommendations) < N:
+                    Nrecommendations.append((item, itemSet[item]))
+                else:
+                    break
+
+            Nrecommendations.sort(key=lambda d: d[1], reverse=True)
+            recommendations = [item[1] for item in Nrecommendations]
+            resNames = [item[0] for item in Nrecommendations]
+
+            # find the K biggest scores
+            for item in itemSet:
+                ind = N
+                l = 0
+                r = N - 1
+
+                if recommendations[r] < itemSet[item]:
+                    while True:
+                        mid = int((l + r) / 2)
+                        if recommendations[mid] >= itemSet[item]:
+                            l = mid + 1
+                        elif recommendations[mid] < itemSet[item]:
+                            r = mid - 1
+                        if r < l:
+                            ind = r
+                            break
+                # ind = bisect(recommendations, itemSet[item])
+                if ind < N - 1:
+                    recommendations[ind + 1] = itemSet[item]
+                    resNames[ind + 1] = item
+            recList[user] = resNames
+        measure = Measure.rankingMeasure(testSample, recList, [10], itemcount)
+        print ('-'*80)
+        print ('Ranking Performance '+self.foldInfo+' (Top-10 On 300 sampled users)')
+        for m in measure[1:]:
+            print (m.strip())
+        print ('-'*80)
+        return measure
+
+    
