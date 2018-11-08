6766690c31e7ad8fdbbe47c93e408c1e2ab987e7
Junwei Zhang <474650206@qq.com>
2018-11-08 21:16:19 +0800

    Update the BPR to the tensorflow version

--- recommender/cf/BPR.py
+++ recommender/cf/BPR.py
@@ -7,6 +7,12 @@ from random import choice
 from tool.qmath import sigmoid
 from math import log
 from collections import defaultdict
+import tensorflow as tf
+from tensorflow import set_random_seed
+import random
+
+set_random_seed(2)
+
 class BPR(IterativeRecommender):
 
     # BPR：Bayesian Personalized Ranking from Implicit Feedback
@@ -17,14 +23,17 @@ class BPR(IterativeRecommender):
 
     def initModel(self):
         super(BPR, self).initModel()
+        self.m = self.data.getSize('user')
+        self.n = self.data.getSize(self.recType)
+        self.train_size = len(self.data.trainingData)
 
-
+    '''
     def buildModel(self):
         userListen = defaultdict(dict)
         for user in self.data.userRecord:
             for item in self.data.userRecord[user]:
                 userListen[user][item[self.recType]] = 1
-
+        
         print ('training...')
         iteration = 0
         itemList = list(self.data.name2id[self.recType].keys())
@@ -51,7 +60,74 @@ class BPR(IterativeRecommender):
             iteration += 1
             if self.isConverged(iteration):
                 break
+    '''
 
+    def next_batch(self):
+        batch_idx = np.random.randint(len(self.data.trainingData), size=512)
+        users = [self.data.trainingData[idx]['user'] for idx in batch_idx]
+        items = [self.data.trainingData[idx]['track'] for idx in batch_idx]
+        user_idx,item_idx=[],[]
+        neg_item_idx = []
+        for i,user in enumerate(users):
+            uid = self.data.getId(user, 'user')
+            for j in range(100): #negative sampling
+                item_j = random.randint(0, self.n- 1)
+                while item_j in self.userListen[uid]:
+                    item_j = random.randint(0, self.n - 1)
+                tid = self.data.getId(items[i], 'track')
+                user_idx.append(uid)
+                item_idx.append(tid)
+                neg_item_idx.append(item_j)
+        return user_idx, item_idx, neg_item_idx
+
+    def buildModel(self):
+        self.userListen = defaultdict(dict)
+        for user in self.data.userRecord:
+            uid = self.data.getId(user, 'user')
+            for item in self.data.userRecord[user]:
+                iid = self.data.getId(item[self.recType], 'track')
+                if item[self.recType] not in self.userListen[user]:
+                    self.userListen[uid][iid] = 0
+                self.userListen[uid][iid] += 1   
+
+        self.u_idx = tf.placeholder(tf.int32, [None], name="u_idx")
+        self.v_idx = tf.placeholder(tf.int32, [None], name="v_idx")
+        self.neg_idx = tf.placeholder(tf.int32, [None], name="n_idx")
+
+        self.U = tf.Variable(tf.truncated_normal(shape=[self.m, self.k], stddev=0.005), name='U')
+        self.V = tf.Variable(tf.truncated_normal(shape=[self.n, self.k], stddev=0.005), name='V')
+
+        self.U_embed = tf.nn.embedding_lookup(self.U, self.u_idx)
+        self.V_embed = tf.nn.embedding_lookup(self.V, self.v_idx)
+        self.V_neg_embed = tf.nn.embedding_lookup(self.V, self.neg_idx)
+        # 构造损失函数 设置优化器
+        self.reg_lambda = tf.constant(self.regU, dtype=tf.float32)
+      
+        error = tf.subtract(tf.reduce_sum(tf.multiply(self.U_embed, self.V_embed), 1), tf.reduce_sum(tf.multiply(self.U_embed, self.V_neg_embed), 1))
+        self.loss = tf.reduce_sum(tf.nn.softplus(-error))
+        # 构造正则化项 完善损失函数
+        self.reg_loss = tf.add(tf.multiply(self.reg_lambda, tf.nn.l2_loss(self.U_embed)),
+                               tf.multiply(self.reg_lambda, tf.nn.l2_loss(self.V_embed)))
+        self.reg_loss = tf.add(tf.multiply(self.reg_lambda, tf.nn.l2_loss(self.V_neg_embed)), self.reg_loss)
+        self.total_loss = tf.add(self.loss, self.reg_loss)
+       
+        self.optimizer = tf.train.AdamOptimizer(self.lRate)
+        self.train = self.optimizer.minimize(self.total_loss)
+        # 初始化会话
+        with tf.Session() as sess:
+            init = tf.global_variables_initializer()
+            sess.run(init)
+            # 迭代，传递变量
+            for epoch in range(self.maxIter):
+                # 按批优化
+                user_idx, item_idx, neg_item_idx = self.next_batch()
+                _,loss = sess.run([self.train,self.total_loss],feed_dict={self.u_idx: user_idx, self.v_idx: item_idx, self.neg_idx:neg_item_idx})
+                print ('iteration:', epoch, 'loss:',loss)
+                # 输出训练完毕的矩阵
+                self.P = sess.run(self.U)
+                self.Q = sess.run(self.V)
+                self.ranking_performance()
+    
     def predict(self, u):
         'invoked to rank all the items for the user'
         u = self.data.getId(u,'user')
